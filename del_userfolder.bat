409b5bdf379e0874de9405557186e1a4e3ac3531
Patrick Veronneau <patrick@decepticons.ca>
2018-03-08 20:36:47 -0700

    Updated to add frame counter and new install directory

--- del_userfolder.bat
+++ del_userfolder.bat
@@ -1,6 +1,7 @@
-SET scdir=C:\Program Files\Roberts Space Industries\StarCitizen
+SET scdir=C:\Program Files\Roberts Space Industries\StarCitizenPTU
 SET cfgbkpdir=%UserProfile%\Documents\SC_config_backup
 SET cntrloffset=LIVE\USER\Controls\Mappings\
 xcopy "%scdir%\%cntrloffset%\*" "%cfgbkpdir%\" /F /R /Y /I
 rmdir /s "%scdir%\LIVE\USER"
 xcopy "%cfgbkpdir%\*" "%scdir%\%cntrloffset%\"  /F /R /Y /I
+echo r_displayinfo=3 > "%scdir%\LIVE\USER.cfg"
