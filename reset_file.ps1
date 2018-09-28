5a3e3b242a0c578da6bc84fb7fe963f74ec651e0
Patrick Veronneau <patrick@decepticons.ca>
2018-09-28 17:22:08 -0600

    Changed backup folder

--- reset_file.ps1
+++ reset_file.ps1
@@ -34,14 +34,17 @@ $scdir = $installdir.Replace("RSI Launcher","")
 # Make a backup of all the exported xml maps also Make sure the backup directories exist, if they don't create them
 if (Test-Path -path $puxmlsource) {if (!(Test-Path -path $cfgbkpdir\PU\)) {New-Item $cfgbkpdir\PU\ -Type Directory} Copy-item $puxmlsource\* -Destination $cfgbkpdir\PU\ -Force -Recurse}
 if (Test-Path -path $ptuxmlsource) {if (!(Test-Path -path $cfgbkpdir\PTU\)) {New-Item $cfgbkpdir\PTU\ -Type Directory} Copy-item $ptuxmlsource\* -Destination $cfgbkpdir\PTU\ -Force -Recurse}
+if (Test-Path -path $ptuptuxmlsource) {if (!(Test-Path -path $cfgbkpdir\PTU_Launcher_PTU\)) {New-Item $cfgbkpdir\PTU_Launcher_PTU\ -Type Directory} Copy-item $ptuptuxmlsource\* -Destination $cfgbkpdir\PTU_Launcher_PTU\ -Force -Recurse}
 # Delete the user folder
 if (Test-Path -path $puuser) { Remove-Item -Recurse -Force -LiteralPath $puuser }
 if (Test-Path -path $ptuuser) { Remove-Item -Recurse -Force -LiteralPath $ptuuser }
+if (Test-Path -path $ptuptuuser) { Remove-Item -Recurse -Force -LiteralPath $ptuptuuser }
 # Make sure source directories exist, then restore xml files
 if ((Test-Path -path $cfgbkpdir\PU\)) {New-Item $puxmlsource -Type Directory ; Copy-item $cfgbkpdir\PU\* -Destination $puxmlsource -Force -Recurse }
 if ((Test-Path -path $cfgbkpdir\PTU\)) {New-Item $ptuxmlsource -Type Directory ; Copy-item $cfgbkpdir\PTU\* -Destination $ptuxmlsource  -Force -Recurse}
-if ((Test-Path -path $cfgbkpdir\PTU\)) {New-Item $ptuxmlsource -Type Directory ; Copy-item $cfgbkpdir\PTU\* -Destination $ptuptuxmlsource  -Force -Recurse}
+if ((Test-Path -path $cfgbkpdir\PTU_Launcher_PTU\)) {New-Item $ptuptuxmlsource -Type Directory ; Copy-item $cfgbkpdir\PTU_Launcher_PTU\* -Destination $ptuptuxmlsource  -Force -Recurse}
 # Add user.cfg to PTU version
 New-Item -Path $scdir$ptudir\LIVE -Name "USER.cfg" -ItemType "file" -Value $userconfig  -Force
+New-Item -Path $scdir$ptudir\PTU -Name "USER.cfg" -ItemType "file" -Value $userconfig  -Force
 # Add user.cfg to PU version
 #New-Item -Path $scdir$pudir\LIVE -Name "USER.cfg" -ItemType "file" -Value $userconfig  -Force
